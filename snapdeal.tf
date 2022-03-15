e1c754a47867aee0413567099fd80a7e3d63a4aa
GAGAsree <101580021+GAGAsree@users.noreply.github.com>
2022-03-15 16:48:47 +0530

    Update snapdeal.tf

--- snapdeal.tf
+++ snapdeal.tf
@@ -3,7 +3,10 @@ provider "aws" {
   access_key = "AKIAVMFWVN35PU6A6M4R"
   secret_key = "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"
 }
-resource "aws_s3_bucket" "def"
-bucket = "sulaks456sulaks"
-acl = "private"
+resource "aws_opsworks_rds_db_instance" "my_instance" {
+  stack_id            = aws_opsworks_stack.my_stack.id
+  rds_db_instance_arn = aws_db_instance.my_instance.arn
+  db_user             = "subhashini"
+  db_password         = "asdf1234"
+}
 }
